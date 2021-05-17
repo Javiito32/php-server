d904a5ac61eac34b81aca6588883b009e261f7ae
Javier <42683930+Javiito32@users.noreply.github.com>
2021-05-17 12:38:06 +0200

    Added Database Name

--- www/connection/connection.php
+++ www/connection/connection.php
@@ -3,7 +3,7 @@ $servername = "10.5.0.20"; //Don't change this unless you know exactly what you'
 $username = "root";
 $password = "contrasenya";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, "mainDB");
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
